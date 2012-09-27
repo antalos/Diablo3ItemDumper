77eea2bcea2161fbacae9644c78a30a247786abd
antalos <antalos@gmail.com>
2012-09-27 12:43:58 +0400

    hotkey binding

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,7 +2,7 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'd3 Item Dumper'
-  ClientHeight = 316
+  ClientHeight = 466
   ClientWidth = 315
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -15,7 +15,7 @@ object Form1: TForm1
   OnDestroy = FormDestroy
   DesignSize = (
     315
-    316)
+    466)
   PixelsPerInch = 96
   TextHeight = 13
   object Label1: TLabel
@@ -25,11 +25,25 @@ object Form1: TForm1
     Height = 13
     Caption = 'Press F9 while cursor is over item - the text should be copied '
   end
+  object Label2: TLabel
+    Left = 8
+    Top = 112
+    Width = 72
+    Height = 13
+    Caption = 'Dump Item key'
+  end
+  object Label3: TLabel
+    Left = 8
+    Top = 135
+    Width = 73
+    Height = 13
+    Caption = 'Dump Char key'
+  end
   object Memo1: TMemo
     Left = 0
-    Top = 112
+    Top = 160
     Width = 313
-    Height = 201
+    Height = 298
     Anchors = [akLeft, akTop, akRight, akBottom]
     Lines.Strings = (
       'Memo1')
@@ -42,6 +56,7 @@ object Form1: TForm1
     Height = 25
     Caption = 'dump ui'
     TabOrder = 1
+    Visible = False
     OnClick = Button1Click
   end
   object cbTranslate: TCheckBox
@@ -53,6 +68,31 @@ object Form1: TForm1
     Checked = True
     State = cbChecked
     TabOrder = 2
+  end
+  object hkDumpItem: THotKey
+    Left = 86
+    Top = 112
+    Width = 70
+    Height = 19
+    HotKey = 32833
+    TabOrder = 3
+  end
+  object Button2: TButton
+    Left = 159
+    Top = 107
+    Width = 75
+    Height = 25
+    Caption = 'Save keys'
+    TabOrder = 4
+    OnClick = Button2Click
+  end
+  object hkDumpChar: THotKey
+    Left = 87
+    Top = 135
+    Width = 70
+    Height = 19
+    HotKey = 32833
+    TabOrder = 5
   end
   object ApplicationEvents1: TApplicationEvents
     OnMinimize = ApplicationEvents1Minimize
--- Unit1.pas
+++ Unit1.pas
@@ -3,12 +3,14 @@ unit Unit1;
 interface
 
 uses
-  functions, Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, mem_reader, ClipBrd, ShellApi, AppEvnts, translator, d3const, d3ui;
+  functions, Windows, Messages, SysUtils, Variants, Classes, Graphics,
+  Controls, Forms, Menus,
+  Dialogs, StdCtrls, mem_reader, ClipBrd, ShellApi, AppEvnts, translator,
+  d3const, d3ui,
+  ComCtrls, IniFiles;
 
 const
-  WM_NOTIFYICON  = WM_USER+333;
-
+  WM_NOTIFYICON = WM_USER + 333;
 
 type
   TForm1 = class(TForm)
@@ -17,10 +19,16 @@ type
     Button1: TButton;
     ApplicationEvents1: TApplicationEvents;
     cbTranslate: TCheckBox;
+    hkDumpItem: THotKey;
+    Button2: TButton;
+    hkDumpChar: THotKey;
+    Label2: TLabel;
+    Label3: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure ApplicationEvents1Minimize(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
 
   private
     { Private declarations }
@@ -28,74 +36,140 @@ type
     HMainIcon: HICON;
 
     procedure CMClickIcon(var msg: TMessage); message WM_NOTIFYICON;
-    procedure WMHotkey( var msg: TWMHotkey ); message WM_HOTKEY;
+    procedure WMHotkey(var msg: TWMHotkey); message WM_HOTKEY;
 
   public
     { Public declarations }
   end;
 
   THashRec = record
-    hash1, hash2 : dword;
+    hash1, hash2: dword;
   end;
-
-
-
 
 var
   Form1: TForm1;
 
-  hashrec : array[0..1024] of THashRec;
-  client_hwnd : hwnd;
+  hashrec: array [0 .. 1024] of THashRec;
+  client_hwnd: hwnd;
 
 const
 
   CUR_VERSION = '104e';
 
-
-  procedure log(s:string);
-
-
+procedure log(s: string);
 
 implementation
 
 {$R *.dfm}
 
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  s, ibind, cbind: string;
+  f : TIniFile;
+  Shortcut: TShortCut;
+  Flags: Cardinal;
+  Key  : Word;
+  Shift : TShiftState;
+begin
+  top := 10;
+  left := screen.Width - Width - 10;
+  Memo1.Lines.Clear();
+
+  Label1.Caption :=
+    'Press F11 while the cursor is over item - the item info should '#13#10'be copied to clipboard and it will also apper in textbox below'#13#10'use ctrl+F11 to dump character''s stats';
+  Label1.Caption := Label1.Caption + #13#10#13#10'my skype: antalos22';
+  Label1.Caption := Label1.Caption + #13#10#13#10'kudos to leprosorium.ru';
+
+  HMainIcon := LoadIcon(MainInstance, 'MAINICON');
+
+  Shell_NotifyIcon(NIM_DELETE, @tnid);
+
+  tnid.cbSize := sizeof(TNotifyIconData);
+  tnid.Wnd := Handle;
+  tnid.uID := 123;
+  tnid.uFlags := NIF_MESSAGE or NIF_ICON or NIF_TIP;
+  tnid.uCallbackMessage := WM_NOTIFYICON;
+  tnid.HICON := HMainIcon;
+  tnid.szTip := 'POP3 Server';
+
+  Shell_NotifyIcon(NIM_ADD, @tnid);
+
+  Memo1.Lines.Add('ver: ' + CUR_VERSION);
+  // memo1.Lines.Clear();
+
+  f := TIniFile.Create(ExtractFilePath(Application.ExeName) + '\app.ini');
+  ibind  := f.ReadString('common', 'itemDumpKey', 'f11');
+  cbind := f.ReadString('common', 'charDumpKey', 'Ctrl+f11');
+  f.Free;
+
+  hkDumpItem.HotKey := TextToShortCut( ibind );
+  hkDumpChar.HotKey := TextToShortCut( cbind );
+
+  ShortCut := TextToShortCut(ibind);
+  Flags := 0;
+  Key   := 0;
+  Shift  := [];
+  ShortCutToKey(Shortcut, Key, Shift);
+  if ssCtrl in Shift then Flags := Flags or MOD_CONTROL;
+  if ssShift in Shift then Flags := Flags or MOD_SHIFT;
+  if ssAlt in Shift then  Flags := Flags or MOD_ALT;
+  if not RegisterHotkey(Handle, 1, Flags, Key) then log('[ERROR] Unable to assign '+ibind+' as hotkey.');
+
+  ShortCut := TextToShortCut(cbind);
+  Flags := 0;
+  Key   := 0;
+  Shift  := [];
+  ShortCutToKey(Shortcut, Key, Shift);
+  if ssCtrl in Shift then Flags := Flags or MOD_CONTROL;
+  if ssShift in Shift then Flags := Flags or MOD_SHIFT;
+  if ssAlt in Shift then  Flags := Flags or MOD_ALT;
+  if not RegisterHotkey(Handle, 2, Flags, Key) then log('[ERROR] Unable to assign '+cbind+' as hotkey.');
+
+
+end;
+
+
+procedure TForm1.Button2Click(Sender: TObject);
+var  f : TIniFile;
+begin
+  f := TIniFile.Create(ExtractFilePath(Application.ExeName) + 'app.ini');
+  f.writeString('common', 'itemDumpKey', ShortCutToText( hkDumpItem.HotKey ));
+  f.writeString('common', 'charDumpKey', ShortCutToText( hkDumpChar.HotKey ));
+  f.Free;
+
+  log('!!! WARN !!!! New hotkeys will work after program restart')
+end;
+
+
 procedure TForm1.CMClickIcon(var msg: TMessage);
 begin
   case msg.lparam of
-    WM_LBUTTONDOWN : begin
-      Application.RestoreTopMosts;
-      WindowState := wsNORMAL;
+    WM_LBUTTONDOWN:
+      begin
+        Application.RestoreTopMosts;
+        WindowState := wsNORMAL;
 
-      Visible := true;
-      Show;
-      self.Activate;
-      windows.SetFocus(self.Handle);
-      SetForegroundWindow(self.Handle);
-      application.BringToFront();
+        Visible := true;
+        Show;
+        self.Activate;
+        Windows.SetFocus(self.Handle);
+        SetForegroundWindow(self.Handle);
+        Application.BringToFront();
 
-    end;
+      end;
   end;
 end;
 
-
-procedure log(s:string);
+procedure log(s: string);
 begin
-  form1.Memo1.Lines.Add(s);
+  Form1.Memo1.Lines.Add(s);
 end;
-
-
-
-
-
-
-
 
 procedure TForm1.ApplicationEvents1Minimize(Sender: TObject);
 begin
-//  Action := caNone;
-//  Hide;
-Visible := false;
+  // Action := caNone;
+  // Hide;
+  Visible := false;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -103,77 +177,22 @@ begin
   dump_ui();
 end;
 
-
-
-procedure TForm1.FormCreate(Sender: TObject);
-var s : string;
-begin
-  top := 10;
-  left := screen.Width - width - 10;
-  memo1.Lines.Clear();
-
-  if not RegisterHotkey(Handle, 1, 0, VK_F11) then begin
-    log('[ERROR] Unable to assign f11 as hotkey.');
-  end;
-
-  if not RegisterHotkey(Handle, 2, MOD_CONTROL, VK_F11) then begin
-    log('[ERROR] Unable to assign ctrl+f11 as hotkey.');
-  end;
-
- {* client_hwnd := HWND(FindWindow(nil, PChar('Diablo III')));
-  if not(IsWindow(client_hwnd)) then client_hwnd := 0;
-
-  log('ver: ' + CUR_VERSION);
-  if client_hwnd <> 0 then begin
-    mr := CMem_reader.create(  );
-    mr.attach( client_hwnd );
-  end else begin
-    log('[ERROR] d3 not found');
-    exit;
-  end;
-  *}
-
-  label1.Caption := 'Press F11 while the cursor is over item - the item info should '#13#10'be copied to clipboard and it will also apper in textbox below'#13#10'use ctrl+F11 to dump character''s stats';
-  label1.Caption := label1.Caption+#13#10#13#10'my skype: antalos22';
-  label1.Caption := label1.Caption+#13#10#13#10'kudos to leprosorium.ru';
-
-  HMainIcon                := LoadIcon(MainInstance, 'MAINICON');
-
-  Shell_NotifyIcon(NIM_DELETE, @tnid);
-
-  tnid.cbSize              := sizeof(TNotifyIconData);
-  tnid.Wnd                 := handle;
-  tnid.uID                 := 123;
-  tnid.uFlags              := NIF_MESSAGE or NIF_ICON or NIF_TIP;
-  tnid.uCallbackMessage    := WM_NOTIFYICON;
-  tnid.hIcon               := HMainIcon;
-  tnid.szTip               := 'POP3 Server';
-
-  Shell_NotifyIcon(NIM_ADD, @tnid);
-
-  memo1.Lines.Add('ver: '+CUR_VERSION);
-//  memo1.Lines.Clear();
-end;
-
-
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
- Shell_NotifyIcon(NIM_DELETE, @tnid);
+  Shell_NotifyIcon(NIM_DELETE, @tnid);
 end;
 
-
-
-
-procedure TForm1.WMHotkey( var msg: TWMHotkey );
-  var s : string;
+procedure TForm1.WMHotkey(var msg: TWMHotkey);
+var
+  s: string;
 begin
   if msg.hotkey = 1 then
   begin
-    //Button1Click(self);
+    // Button1Click(self);
 
-    //dump_ui();    exit;
+    // dump_ui();    exit;
     s := get_info_from_itempopup();
-    log( s );
+    log(s);
     log('-----------------------');
     Clipboard.AsText := s;
     exit;
@@ -181,9 +200,9 @@ begin
 
   if msg.hotkey = 2 then
   begin
-    //s := get_short_charinfo();
+    // s := get_short_charinfo();
     s := get_full_charinfo();
-    log( s );
+    log(s);
     log('-----------------------');
     Clipboard.AsText := s;
     exit;
